0653978fd7dc0e50ec5344c85250ca5faa5725b6
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-02-01 12:47:21 +1100

    default sorting added to the table

--- client/src/components/LogList/LogList.js
+++ client/src/components/LogList/LogList.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 import Return from "../Return/Return"
  
 function LogList(props) {
+    const [list, setList] = useState([])
     console.log(props.results)
     
+    useEffect(() => {
+        setList(props.results)
+    })
+
+    //columns fo the table
     const columns = ["Date", "Time", "logCategory", "Actions"]
+
+    //when loads, the log is displayed in the chronological order 
+    const filteredList = list.sort((a,b) => (a.date > b.date) ? 1 : -1)
+
+    //load the babyID from the localstorage to get the right set of data
     var thisBabyId = JSON.parse(localStorage.getItem("babyClicked"))._id
+
 
     
     return (
         <div>
             <table striped>
-                <thead>
-                    {columns.map((each, i) => {
+                 <thead>
+                     {columns.map((each, i) => {
                        return <th key={i}>{each}</th>
                     })}
                 </thead>
 
                 <tbody>
-                    {props.results.map((each ,i) => {
+                    {filteredList.map((each ,i) => {
                      if (each.babyId === thisBabyId){
                            return <tr key={i}>
                                 <td>{each.date}</td>
