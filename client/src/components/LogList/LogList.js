462adb9565239c981e8f5954fbed263910de7480
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2021-02-01 12:26:24 +1100

    table created for All Records page

--- client/src/components/LogList/LogList.js
+++ client/src/components/LogList/LogList.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import Return from "../Return/Return"
-
+ 
 function LogList(props) {
     console.log(props.results)
     
+    const columns = ["Date", "Time", "logCategory", "Actions"]
     var thisBabyId = JSON.parse(localStorage.getItem("babyClicked"))._id
-    console.log(thisBabyId)
 
+    
     return (
         <div>
-            <ul>
-            {props.results.map((each,i) => {
-                if (each.babyId === thisBabyId){
-                 return <li key={i}>{each.date} {each.logCategory}</li>
-            }})}
-            </ul>
+            <table striped>
+                <thead>
+                    {columns.map((each, i) => {
+                       return <th key={i}>{each}</th>
+                    })}
+                </thead>
+
+                <tbody>
+                    {props.results.map((each ,i) => {
+                     if (each.babyId === thisBabyId){
+                           return <tr key={i}>
+                                <td>{each.date}</td>
+                                <td>{each.time}</td>
+                                <td>{each.logCategory}</td>
+                                <td><button>Edit</button><button>delete</button></td>
+                            </tr>
+                    }})}
+                </tbody>
+
+            </table>
+
+
 
             <Return />
         </div>
